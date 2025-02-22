7f2f502249a9f62d447b65073d1f6917c33d90b4
rashmikaz <phrashmika@gmail.com>
2025-02-22 23:57:38 +0530

    server.ts

--- server.ts
+++ server.ts
@@ -1,8 +1,19 @@
 import express from "express";
+import studentRoutes from "./routes/Student-routes";
 
 const app = express();
+var cors = require("cors")
 
 app.use(express.json());
+app.use(cors());
+
+const corsOption={
+    origin: 'http://localhost:5173',
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOption));
+
+app.use("/Student",studentRoutes);
 
 app.listen(3000,(err=>{
     console.log("server port 3000") ;
