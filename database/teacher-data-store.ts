939bcb86ab7e69e698705be05c3a060a0fbb0860
rashmikaz <phrashmika@gmail.com>
2025-02-23 13:09:26 +0530

    teacher update in data-store

--- database/teacher-data-store.ts
+++ database/teacher-data-store.ts
@@ -30,3 +30,21 @@ export async function getAllTeachers(){
         console.log("error getting teachers from prisma data",err);
     }
 }
+export async function TeacherUpdate(email: string, t: Teacher){
+    try{
+        await prisma.teacher.update({
+            where:{ email : t.email},
+            data:{
+                name: t.name,
+                nic: t.nic,
+                email: t.email,
+                phone: t.phone,
+                address:t.address,
+                subject:t.subject
+
+            }
+        })
+    }catch(err){
+        console.log("error updating teacher", err);
+    }
+}
